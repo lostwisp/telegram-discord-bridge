79ce0f9b345f5f62479a12bdc2d782f38fe4b0b4
lostwisp <axyashine@gmail.com>
2025-09-22 21:20:11 +0300

    added .env

--- bd/postgre.go
+++ bd/postgre.go
@@ -1,6 +1,10 @@
 package bd
 
-import "github.com/jackc/pgx/v5"
+import (
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
-type Client interface {
+type Storage struct {
+	db *pgxpool.Pool
 }
