2ca1f22f928d317b665b7cc00c1847ab8494f7a6
lostwisp <axyashine@gmail.com>
2026-03-09 22:32:09 +0300

    Added discord-bot

--- discord-bot/discord.go
+++ discord-bot/discord.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 	tgdis "github.com/lostwisp/telegram-discord-bridge/gRPC/tg-dis"
-	config "github.com/lostwisp/telegram-discord-bridge/pwd"
 	"google.golang.org/grpc"
 )
 
@@ -19,35 +21,14 @@ func (s *server) NewMessage(ctx context.Context, req *tgdis.MessageRequest) (*tg
 	return &tgdis.MessageResponse{Score: 0}, nil
 }
 
-var (
-	DiscordToken     string
-	DiscordchannelID string
-)
+var bot *discordgo.Session
 
-var Session *discordgo.Session
-
-func InstalDiscordBot() error {
-	var err error
-	Session, err = discordgo.New("Bot " + config.DiscordToken)
-	if err != nil {
-		return err
-	}
-	err = Session.Open()
-	if err != nil {
-		print(err)
-	}
-	return nil
-
-}
-
-func SendMessengeToDiscord(massageTG string) {
-	_, err := Session.ChannelMessageSend(config.DiscordchannelID, massageTG)
-	if err != nil {
-		print(err)
-	}
+func GetEnv() string {
+	return os.Getenv("DISCORD_BOT_TOKEN")
 }
 
 func main() {
+	//Создание сервера для прослушивания gRPC
 	conn, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Println(err)
@@ -59,5 +40,37 @@ func main() {
 
 	if err := grpcServer.Serve(conn); err != nil {
 		log.Fatalf("failed to serve: %v", err)
+	}
+
+	//Создание бота с WebSocket
+	token := GetEnv()
+	bot, err = discordgo.New("Bot " + token)
+	if err != nil {
+		log.Println(err)
+	}
+	bot.AddHandler(pingPong)
+	err = bot.Open()
+	if err != nil {
+		log.Println(err)
+	}
+	defer bot.Close()
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM)
+	<-sc
+}
+
+func pingPong(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Игнорируем сообщения от самого бота (чтобы избежать циклов)
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	// Если сообщение содержит "!ping", отвечаем "Pong!"
+	if m.Content == "!ping" {
+		_, err := s.ChannelMessageSend(m.ChannelID, "Pong! 🏓")
+		if err != nil {
+			log.Printf("Ошибка при отправке сообщения: %v", err)
+		}
 	}
 }
--- discord-bot/go.mod
+++ discord-bot/go.mod
@@ -5,6 +5,7 @@ go 1.25.1
 require (
 	github.com/bwmarrin/discordgo v0.29.0
 	github.com/lostwisp/telegram-discord-bridge v0.0.0-20260115174217-8f158edc7757
+	google.golang.org/grpc v1.78.0
 )
 
 require (
@@ -14,6 +15,5 @@ require (
 	golang.org/x/sys v0.38.0 // indirect
 	golang.org/x/text v0.31.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20251029180050-ab9386a59fda // indirect
-	google.golang.org/grpc v1.78.0 // indirect
 	google.golang.org/protobuf v1.36.11 // indirect
 )
